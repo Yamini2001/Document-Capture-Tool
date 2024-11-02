90e00b63cbeae1118f68603a76a5f8877778d84f
BeSingularTeam <besingulardemo@gmail.com>
2024-11-02 22:12:09 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import DocumentUpload from './documentUpload';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Document Capture Prototype</h1>
+            <DocumentUpload />
+        </div>
+    );
 }
 
 export default App;
